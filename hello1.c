7cc1930caf071d579c98a0ce21b69eeb7c29b84c
RW <rinze.watanabe.translation@gmail.com>
2019-07-17 23:34:35 +1000

    Added snippets

---
+++ hello1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main(){
+    int num1 = 4, num2 = 9;
+    printf("Is 4 > 8 : %d", num1 > num2);
+
+    return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include <string.h>
+
+#define MYNAME "RW"
+int globalVar = 100;
+
+int main(void){
+  char firstLetter = 'R';
+  char someString[] = 'hello';
+  strcpy('Hello', someString);
+
+  int a = 12, b = 1, c = 2;
+
+  // conversion character
+  printf("Hello world\n%d", age);
+
+  return 0;
+}
