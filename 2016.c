149fa44a67c86d87902c2f2fb142f01532fa7948
Rinze Watanabe <rinze.watanabe.translation@gmail.com>
2019-10-28 17:02:22 +1000

    Update

---
+++ 2016.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int sumline(const char *line) {
+
+}
+
+
+int main(int argc, char **argv){
+    printf("%d", sumline("12 2 3 5"));
+}
